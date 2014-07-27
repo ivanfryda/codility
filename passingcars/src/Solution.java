de89e63f95b05d70f1e61e0a2515a33ef37a1d42
Ivan <Ivan@Ivan-PC>
2014-07-27 14:14:51 +0200

    Adding solution.

---
+++ passingcars/src/Solution.java
@@ -0,0 +1,20 @@
+// you can also use imports, for example:
+// import java.util.*;
+
+// you can use System.out.println for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class Solution {
+    public int solution(int[] A) {
+        int travellingEast = 0;
+        int passing = 0;
+        for (int a : A) {
+            if (a == 0) {
+                travellingEast++;
+            } else if (a == 1) {
+                passing += travellingEast;
+            }
+        }
+        return passing;
+    }
+}
